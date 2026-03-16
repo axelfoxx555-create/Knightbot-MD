5a5554d6e3637c6601e2e7b6f75da6f4c6d5a24c
axelfoxx555-create <axelfoxx555@gmail.com>
2026-03-16 19:15:35 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'Knight Bot',
+  packname: 'yandere Bot',
   author: '‎',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '919876543210', //Set your number here without + symbol, just add country code & number without any space
+  botName: "Yuno",
+  botOwner: 'Unholy Father', 
+  ownerNumber: '27670728503',
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   maxStoreMessages: 20, 
